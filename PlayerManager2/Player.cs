91efb0100da0ec4382d2b2b5d29774d5388e14d1
RicardoMota22 <ricardomota.artist@gmail.com>
2025-04-30 22:51:51 +0100

    create PlayerManager2

---
+++ PlayerManager2/Player.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManager1
+{
+    public class Player
+    {
+        //Properties
+        public string Name { get;}
+        public int Score { get; set;}
+
+        //Constructor
+        public Player(string name, int score)
+    {
+        Name = name;
+        Score = score;
+    }
+    }
+}
