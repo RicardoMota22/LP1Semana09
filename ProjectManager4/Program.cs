d442b4a31dee262b0b7c22267a2a4950e09704e8
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-01 22:33:14 +0100

    add ProjectManager4, add class

---
+++ ProjectManager4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectManager4
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
