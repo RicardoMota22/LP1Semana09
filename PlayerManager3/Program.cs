dbc747bb403902222117a97d663c6a17626fd51f
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-01 22:02:02 +0100

    add ProjectManager3, before changes

---
+++ PlayerManager3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PlayerManager3
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello LP1!");
+        }
+    }
+}
