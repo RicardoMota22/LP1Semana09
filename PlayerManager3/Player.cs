c16873366986aee60bdd0ea8aba0789012515f73
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-01 22:18:34 +0100

    add ProjectManager3, desendente

--- PlayerManager3/Player.cs
+++ PlayerManager3/Player.cs
@@ -5,8 +5,24 @@ using System.Threading.Tasks;
 
 namespace PlayerManager3
 {
-    public class Player
+    public class Player : IComparable<Player>
     {
-        
+        //Properties
+        public string Name { get;}
+        public int Score { get; set;}
+
+        //Constructor
+        public Player(string name, int score)
+    {
+        Name = name;
+        Score = score;
+    }
+        //Interface IComparable<T>
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 1;
+            return Score.CompareTo(other.Score);
+        }
+
     }
 }
