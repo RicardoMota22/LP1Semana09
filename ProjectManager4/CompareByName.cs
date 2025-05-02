91a46c0459691741b341bfd6966677c6d979abe5
RicardoMota22 <ricardomota.artist@gmail.com>
2025-05-02 09:29:11 +0100

    add ProjectManager4

--- ProjectManager4/CompareByName.cs
+++ ProjectManager4/CompareByName.cs
@@ -2,11 +2,42 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using PlayerManager4;
 
 namespace ProjectManager4
 {
     public class CompareByName
     {
-        
+        // CompareByName
+        private readonly bool order;
+
+        public CompareByName(bool aorder)
+        {
+            this.order = aorder;
+        }
+        public int Compare(Player x, Player y)
+        {
+            int comparison = string.Compare(x.Name, y.Name, StringComparison.OrdinalIgnoreCase);
+            if (order == true)
+            {
+                return comparison;
+            }
+            else
+            {
+                return -comparison;
+            }
+            
+        }
+
+        public class CompareByScore : IComparer<Player>
+        {
+            public int Compare(Player x, Player y)
+            {
+                if (x == null || y == null) return 0;
+
+                return y.Score.CompareTo(x.Score); // Descendente
+            }
+        }
+
     }
 }
--- ProjectManager4/Player.cs
+++ ProjectManager4/Player.cs
@@ -3,10 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProjectManager4
+namespace PlayerManager4
 {
-    public class Player
+    public class Player : IComparable<Player>
     {
-        
+        //Properties
+        public string Name { get;}
+        public int Score { get; set;}
+
+        //Constructor
+        public Player(string name, int score)
+    {
+        Name = name;
+        Score = score;
+    }
+        //Interface IComparable<T>
+        public int CompareTo(Player other)
+        {
+            if (other == null) return 1;
+            //return Score.CompareTo(other.Score);//ascendente
+            return other.Score.CompareTo(Score);
+        }
+
     }
 }
